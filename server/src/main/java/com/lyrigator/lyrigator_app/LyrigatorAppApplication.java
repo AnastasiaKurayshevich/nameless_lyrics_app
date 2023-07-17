c3d41bd55743fd4beb3209b7e9001ad9582e8ad9
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-17 09:23:44 +0200

    Initialized spring project

---
+++ server/src/main/java/com/lyrigator/lyrigator_app/LyrigatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.lyrigator.lyrigator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyrigatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyrigatorAppApplication.class, args);
+	}
+
+}
