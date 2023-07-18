f6dc097e509f7bf5e7f15bfe29d747cb88a581f2
EdwardArvinius <edward.arvinius@appliedtechnology.se>
2023-07-18 10:22:13 +0200

    changed OPENAI_API_KEY location from .env to secrets.properties

--- server/src/main/java/com/lyrigator/lyrigator_app/openAi/OpenAiClient.java
+++ server/src/main/java/com/lyrigator/lyrigator_app/openAi/OpenAiClient.java
@@ -2,7 +2,6 @@ package com.lyrigator.lyrigator_app.openAi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,21 +11,41 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Component
 public class OpenAiClient {
 
     private final String API_KEY;
     ObjectMapper objectMapper = new ObjectMapper();
     CompletionPayload payload = new CompletionPayload(
-            "text-davinci-002", "Once upon a time", 100, 0.5, 1, 1
+            "text-davinci-002", "Once upon a time", 10, 0.5, 1, 1
     );
     String jsonPayload = objectMapper.writeValueAsString(payload);
 
 
     @Autowired
     public OpenAiClient() throws JsonProcessingException {
-        Dotenv dotenv = Dotenv.load();
-        API_KEY = dotenv.get("OPENAI_API_KEY");
+        API_KEY = loadApiKeyFromProperties("secrets.properties", "OPENAI_API_KEY");
+    }
+
+    private String loadApiKeyFromProperties(String propertiesFileName, String apiKeyProperty) {
+        Properties prop = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
+
+        if (inputStream != null) {
+            try {
+                prop.load(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load properties from file: " + propertiesFileName, e);
+            }
+        } else {
+            throw new RuntimeException("Property file '" + propertiesFileName + "' not found in the classpath");
+        }
+
+        return prop.getProperty(apiKeyProperty);
     }
 
     public String makePostRequest() {
@@ -54,6 +73,85 @@ public class OpenAiClient {
 
         return null;
     }
-
-
 }
+
+
+
+
+//package com.lyrigator.lyrigator_app.openAi;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.reactive.function.BodyInserters;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClientResponseException;
+//import reactor.core.publisher.Mono;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.Properties;
+//
+//@Component
+//public class OpenAiClient {
+//
+//    private final String API_KEY;
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    CompletionPayload payload = new CompletionPayload(
+//            "text-davinci-002", "Once upon a time", 10, 0.5, 1, 1
+//    );
+//    String jsonPayload = objectMapper.writeValueAsString(payload);
+//
+//
+//    @Autowired
+//    public OpenAiClient() throws JsonProcessingException {
+//        Properties prop = new Properties();
+//        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("secrets.properties");
+//
+//        if (inputStream != null) {
+//            try {
+//                prop.load(inputStream);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            System.err.println("Property file 'secrets.properties' not found in the classpath");
+//            System.exit(1);
+//        }
+//
+//        API_KEY = prop.getProperty("OPENAI_API_KEY");
+//    }
+//
+//    public String makePostRequest() {
+//        WebClient webClient = WebClient.builder()
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY)
+//                .build();
+//        try {
+//            Mono<String> responseMono = webClient.post()
+//                    .uri("https://api.openai.com/v1/completions")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .body(BodyInserters.fromValue(jsonPayload))
+//                    .retrieve()
+//                    .bodyToMono(String.class);
+//            String response = responseMono.block();
+//            System.out.println(response);
+//            return response;
+//        } catch (WebClientResponseException e) {
+//            int statusCode = e.getStatusCode().value();
+//            String responseBody = e.getResponseBodyAsString();
+//            System.err.println("HTTP Error " + statusCode + ": " + responseBody);
+//        } catch (Exception e) {
+//            System.err.println("Unexpected error: " + e.getMessage());
+//        }
+//
+//        return null;
+//    }
+//}
+//
+//
+//
+//
