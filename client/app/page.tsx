87d7db453285920a5f70e6b90b1e7fc0c5d57c39
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-18 15:21:16 +0200

    Refactoring fetch

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,20 +1,68 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useState } from 'react';
 
-const getInfo = async () => {
-  const response = await fetch('https://lyrigator.azurewebsites.net/api');
-  const json = await response.text();
-  return json;
+
+export default function Home() {
+
+  const [test, setTest] = useState('');
+
+  useEffect(() => {
+    const getInfo = async () => {
+      try {
+        const response = await fetch('https://lyrigator.azurewebsites.net/api');
+        if (response.ok) {
+          const text = await response.text();
+          setTest(text);
+        } else {
+          console.error('Error:', response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    getInfo();
+  }, []);
+
+  const [inputValue, setInputValue] = useState("");
   
-}
 
-export default async function Home() {
 
-  const testString = await getInfo();
+
+  // const handleSubmit =  async (event: any) => {
+  //   event.preventDefault();
+
+  //  fetch('https://lyrigator.azurewebsites.net/api', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ input: inputValue })
+  //   })
+  //   .then(response => response.text())
+  //   .then(text => console.log(text))
+  //   .catch(error => console.log(error));
+  // };
+
+  // useEffect(() => {
+  //   handleSubmit(event);
+  // }, []);
+  
   return (
-    <main >
+    <>
       <h1>Hello!</h1>
-      <p>{testString}</p>
-      
-    </main>
-  )
-}
+      <p>{test}</p>
+      <div>
+        <form>
+        {/* <form onSubmit={handleSubmit}> */}
+          <input
+          type='text'
+          value={inputValue} 
+          onChange={(e) => setInputValue(e.target.value)}/>
+
+          <input type="submit"/>
+        </form>
+      </div>
+    </>
+  );
+};
