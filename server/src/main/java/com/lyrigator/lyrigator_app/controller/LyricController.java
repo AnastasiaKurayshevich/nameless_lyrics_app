5dbdc81d329950232bb46d0a47262548de5ca5f5
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-18 16:54:50 +0200

    added CORS

--- server/src/main/java/com/lyrigator/lyrigator_app/controller/LyricController.java
+++ server/src/main/java/com/lyrigator/lyrigator_app/controller/LyricController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class LyricController {
     LyricService lyricService;
 
