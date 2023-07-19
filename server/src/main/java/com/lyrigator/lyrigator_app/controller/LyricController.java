8b162c53f8a09d38ebb725602ed2528983cf3951
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-19 11:16:39 +0200

    Commented out everyting with AI to see if app deploys

--- server/src/main/java/com/lyrigator/lyrigator_app/controller/LyricController.java
+++ server/src/main/java/com/lyrigator/lyrigator_app/controller/LyricController.java
@@ -1,7 +1,7 @@
 package com.lyrigator.lyrigator_app.controller;
 
 import com.lyrigator.lyrigator_app.model.Lyric;
-import com.lyrigator.lyrigator_app.openAi.OpenAiClient;
+//import com.lyrigator.lyrigator_app.openAi.OpenAiClient;
 import com.lyrigator.lyrigator_app.service.LyricService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,22 +13,24 @@ import org.springframework.web.bind.annotation.*;
 public class LyricController {
     LyricService lyricService;
 
-    OpenAiClient ai;
+    //OpenAiClient ai;
 
     @Autowired
-    public LyricController(LyricService lyricService, OpenAiClient ai) {
+    public LyricController(LyricService lyricService
+                           //OpenAiClient ai
+                           ) {
 
         this.lyricService = lyricService;
-        this.ai = ai;
+        //this.ai = ai;
     }
 
     @PostMapping
     public ResponseEntity<String> postLyricToDb(@RequestBody Lyric lyric){
         System.out.println(lyric.getTest());
-        String response = ai.makePostRequest(lyric.getTest());
-        System.out.println(response);
+//        String response = ai.makePostRequest(lyric.getTest());
+//        System.out.println(response);
 //        lyricService.saveLyric(lyric);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(lyric.getTest());
     }
 
     @GetMapping
--- server/src/main/java/com/lyrigator/lyrigator_app/openAi/OpenAiClient.java
+++ server/src/main/java/com/lyrigator/lyrigator_app/openAi/OpenAiClient.java
@@ -1,70 +1,70 @@
-
-package com.lyrigator.lyrigator_app.openAi;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
-
-@Component
-@PropertySource("classpath:secrets.properties")
-public class OpenAiClient {
-
-    private final String API_KEY;
-
-
-    private String createJsonPayload(String prompt) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode payload = objectMapper.createObjectNode();
-        payload.put("model", "text-davinci-002");
-        payload.put("prompt", prompt);
-        payload.put("max_tokens", 10);
-        payload.put("temperature", 0.5);
-        payload.put("top_p", 1);
-        payload.put("n", 1);
-        return payload.toString();
-    }
-
-
-    @Autowired
-    public OpenAiClient(@Value("${OPENAI_API_KEY}") String apiKey) {
-        this.API_KEY = apiKey;
-    }
-
-
-    public String makePostRequest(String userInput) {
-        WebClient webClient = WebClient.builder()
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY)
-                .build();
-        try {
-            Mono<String> responseMono = webClient.post()
-                    .uri("https://api.openai.com/v1/completions")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(BodyInserters.fromValue(createJsonPayload(userInput)))
-                    .retrieve()
-                    .bodyToMono(String.class);
-            String response = responseMono.block();
-            System.out.println(response);
-            return response;
-        } catch (WebClientResponseException e) {
-            int statusCode = e.getStatusCode().value();
-            String responseBody = e.getResponseBodyAsString();
-            System.err.println("HTTP Error " + statusCode + ": " + responseBody);
-        } catch (Exception e) {
-            System.err.println("Unexpected error: " + e.getMessage());
-        }
-
-        return null;
-    }
-
-
-}
+//
+//package com.lyrigator.lyrigator_app.openAi;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.node.ObjectNode;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.reactive.function.BodyInserters;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClientResponseException;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+//@PropertySource("classpath:secrets.properties")
+//public class OpenAiClient {
+//
+//    private final String API_KEY;
+//
+//
+//    private String createJsonPayload(String prompt) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        ObjectNode payload = objectMapper.createObjectNode();
+//        payload.put("model", "text-davinci-002");
+//        payload.put("prompt", prompt);
+//        payload.put("max_tokens", 10);
+//        payload.put("temperature", 0.5);
+//        payload.put("top_p", 1);
+//        payload.put("n", 1);
+//        return payload.toString();
+//    }
+//
+//
+//    @Autowired
+//    public OpenAiClient(@Value("${OPENAI_API_KEY}") String apiKey) {
+//        this.API_KEY = apiKey;
+//    }
+//
+//
+//    public String makePostRequest(String userInput) {
+//        WebClient webClient = WebClient.builder()
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY)
+//                .build();
+//        try {
+//            Mono<String> responseMono = webClient.post()
+//                    .uri("https://api.openai.com/v1/completions")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .body(BodyInserters.fromValue(createJsonPayload(userInput)))
+//                    .retrieve()
+//                    .bodyToMono(String.class);
+//            String response = responseMono.block();
+//            System.out.println(response);
+//            return response;
+//        } catch (WebClientResponseException e) {
+//            int statusCode = e.getStatusCode().value();
+//            String responseBody = e.getResponseBodyAsString();
+//            System.err.println("HTTP Error " + statusCode + ": " + responseBody);
+//        } catch (Exception e) {
+//            System.err.println("Unexpected error: " + e.getMessage());
+//        }
+//
+//        return null;
+//    }
+//
+//
+//}
