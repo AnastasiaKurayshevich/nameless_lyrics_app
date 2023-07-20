7ef06d31130cf24ba87152347a4159f51d9dd293
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-20 11:59:14 +0200

    Modified fetch by id in FE

--- client/app/home/[songId]/page.tsx
+++ client/app/home/[songId]/page.tsx
@@ -4,44 +4,50 @@ import React, { useEffect, useState } from 'react'
 type Song = {
     id: number;
     songName: string;
-    listOfSongs: LyricPart[];
-  };
-  
-  type LyricPart = {
-    id: number;
-    lyricTitle: string; 
-    lyric: string;
-  }
+    songList: LyricPart[];
+};
 
-  type Props = {
+type LyricPart = {
+    id: number;
+    lyricTitle: string;
+    lyric: string;
+}
+
+type Props = {
     params: {
-      songId: string;
+        songId: string;
     }
 }
 
-export default function Song (props: Props) {
+export default function Song(props: Props) {
     const [song, setSong] = useState<Song>();
 
 
-  useEffect(() => {
-    const getSongs = async () => {
-      const response = await fetch(`http://localhost:8080/api/songs/${props.params.songId}`);
-      const data = await response.json();
-      setSong(data);
-    };
-    getSongs();
-  }, []);
+    useEffect(() => {
+        const getSongs = async () => {
+            const response = await fetch(`http://localhost:8080/api/songs/${props.params.songId}`);
+            const data = await response.json();
+            console.log(data);
+            setSong(data);
+        };
+        getSongs();
+    }, []);
 
 
-  return (
-    <div>
-        {song?.songName}
-        <ul>{song?.listOfSongs.map((lyricPart: LyricPart) => (
-            <li key={lyricPart.id}>
-                <h3>{lyricPart.lyricTitle}</h3>
-                <h4>{lyricPart.lyric}</h4>
-            </li>
-        ))}</ul>
-    </div>
-  )
+    return (
+        <div>
+            {song?.songName}
+            {song?.songList ? (
+                <ul>{song?.songList.map((lyricPart: LyricPart) => (
+                    <li key={lyricPart.id}>
+                        <h3>{lyricPart.lyricTitle}</h3>
+                        <h4>{lyricPart.lyric}</h4>
+                    </li>
+                ))}
+                </ul>
+            ) : (
+                <p>Loading...</p> // You can display a loading message while fetching the data
+            )}
+        </div>
+    )
 }
