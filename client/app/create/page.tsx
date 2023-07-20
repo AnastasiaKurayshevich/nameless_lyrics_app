cb0bae6c9b4d48d2a5626654bfd49ef1eed12905
Nate Arafayne <nate.arafayne@appliedtechnology.se>
2023-07-20 14:43:47 +0200

    implimented a description textarea

--- client/app/create/page.tsx
+++ client/app/create/page.tsx
@@ -1,87 +1,87 @@
-"use client"
-import { type } from 'os'
-import React, { use, useState } from 'react'
-
+"use client";
+import { type } from "os";
+import React, { use, useState } from "react";
 
 type FormData = {
-    genre?: string;
-    mood?: string;
-    description?: string;
-}
-
+  genre?: string;
+  mood?: string;
+  description?: string;
+};
 
 export default function Create() {
+  const [formData, setFormData] = useState<FormData>({
+    genre: "",
+    mood: "",
+    description: "",
+  });
 
-    const [formData, setFormData] = useState<FormData>({
-        genre: '',
-        mood: '',
-        description: '',
-    });
+  const handleGenreChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormData({ ...formData, genre: event.target.value });
+  };
 
-    const handleGenreChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setFormData({ ...formData, genre: event.target.value });
-    };
+  const handleMoodChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormData({ ...formData, mood: event.target.value });
+  };
 
-    const handleMoodChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setFormData({ ...formData, mood: event.target.value });
-    };
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, description: event.target.value });
+  };
 
-    const handleDescriptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setFormData({ ...formData, description: event.target.value });
-    };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
+    fetch("http://localhost:8080/api/new-song", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ formData }),
+    })
+      .then((response) => response.text())
+      .then((text) => console.log(text))
+      .catch((error) => console.log(error));
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    console.log(formData);
+  };
 
-        fetch('http://localhost:8080/api/new-song', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ formData })
-        })
-            .then(response => response.text())
-            .then(text => console.log(text))
-            .catch(error => console.log(error));
-
-        console.log(formData)
-    }
-
-
-
-
-
-
-    return (
-
-        <div>
-            <h2>Create</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Genre:
-                    <select value={formData.genre} onChange={handleGenreChange}>
-                        <option value=""></option>
-                        <option value="Pop">Pop</option>
-                        <option value="Rock">Rock</option>
-                        <option value="Blues">Blues</option>
-                    </select>
-                </label>
-                <br />
-                <label>
-                    Mood:
-                    <select value={formData.mood} onChange={handleMoodChange}>
-                        <option value=""></option>
-                        <option value="Sad">Sad</option>
-                        <option value="Happy">Happy</option>
-                        <option value="Angsty">Angsty</option>
-                    </select>
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-
-
-    )
+  return (
+    <div>
+      <h2>Create</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Genre:
+          <select value={formData.genre} onChange={handleGenreChange}>
+            <option value=""></option>
+            <option value="Pop">Pop</option>
+            <option value="Rock">Rock</option>
+            <option value="Blues">Blues</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Mood:
+          <select value={formData.mood} onChange={handleMoodChange}>
+            <option value=""></option>
+            <option value="Happy">Happy</option>
+            <option value="Sad">Sad</option>
+            <option value="Angsty">Angsty</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Description:
+          <textarea
+            cols={50}
+            rows={5}
+            value={formData.description}
+            onChange={handleDescriptionChange}
+          ></textarea>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
