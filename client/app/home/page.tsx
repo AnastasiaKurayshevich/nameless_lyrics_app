19d0a1a9e6363e3beb24eaee8511138b254eddbb
Nate Arafayne <nate.arafayne@appliedtechnology.se>
2023-07-20 11:00:27 +0200

    Started on the home page

---
+++ client/app/home/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+type Song = {
+  id: number;
+  songName: string;
+};
+
+export default function Home() {
+  const [songs, setSongs] = useState<any>([]);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      const response = await fetch(`http://localhost:8080/api/songs`);
+      const data = await response.json();
+      setSongs(data);
+    };
+    getSongs();
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h2 className="text-5xl text-center">Your Lyrics</h2>
+      <ul>
+        {songs.map((song: Song) => (
+          <li key={song.id}>
+            <Link href="">{song.songName}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
