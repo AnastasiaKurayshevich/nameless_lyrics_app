50b77984749114d57d424847582bca0c16a32b6e
Nate Arafayne <nate.arafayne@appliedtechnology.se>
2023-07-21 14:22:20 +0200

    made the delete a circle

--- client/app/create/SongStructure.tsx
+++ client/app/create/SongStructure.tsx
@@ -43,13 +43,15 @@ export default function SongStructure({ isVisible, structure, setStructure } : S
         <div key={index}>
           <h3>{part.name}</h3>
           <textarea
+            className="textarea textarea-success"
             cols={40}
             rows={8}
             value={part.lyrics}
             onChange={(event) => handleLyricsChange(event, index)}
           />
-          <button type="button" onClick={() => handleDelete(index)}>
-            Delete
+          <button className='btn btn-circle btn-outline btn-xs' type="button" onClick={() => handleDelete(index)}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+
           </button>
         </div>
       ))}
