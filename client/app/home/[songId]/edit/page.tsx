bcceee5f31c3c1277c0aaf9a8e4783444549939d
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-07-24 10:01:33 +0200

    Passing the id of the song to the edit page

--- client/app/home/[songId]/edit/page.tsx
+++ client/app/home/[songId]/edit/page.tsx
@@ -1,10 +1,18 @@
 "use client"
 import React from 'react';
 
-export default function EditPage() {
+type Props = {
+  params: {
+    songId: string;
+  }
+}
+
+
+export default function EditPage(props: Props) {
   return (
     <div>
       <h1>Edit Page</h1>
+      <h3>this is the song with id {props.params.songId}</h3>
     </div>
   )
 }
