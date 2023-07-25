f0f6bd7b51ba2b86bd20d40fcb62365c4957eb55
Nate Arafayne <nate.arafayne@appliedtechnology.se>
2023-07-25 17:05:13 +0200

    Added css to the first page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,15 +5,15 @@ export default function Start() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse items-center">
-        <img src="/Lyrigator_image_1.png" className="max-w-sm rounded-lg shadow-2xl" />
+        <img src="/Lyrigator_image_1.png" className="rounded-lg " />
         <div>
           <h2 className="text-5xl font-bold">Welcome to <span className="text-accent-focus">Lyrigator</span></h2>
           <p className="py-6">An AI powered lyric generator.</p>
           <Link href="/home">
-            <button className="btn btn-primary">Your Lyrics</button>
+            <button className="btn btn-secondary mr-10">Your Lyrics</button>
           </Link>
           <Link href="/create">
-            <button className="btn btn-secondary">New lyrics</button>
+            <button className="btn btn-primary">New lyrics</button>
           </Link>
         </div>
       </div>
