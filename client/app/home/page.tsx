0b2a611411f4c43f1c66224a59846f5a3a46ddd2
EdwardArvinius <edward.arvinius@appliedtechnology.se>
2023-07-26 23:33:21 +0200

    added styling to your song buttons

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full pb-20">
+      <div className="song-list">
         {isLoading ? (
           // If isLoading is true, display the loading animation
           <div className="flex justify-center">
@@ -102,25 +102,30 @@ export default function Home() {
           <ul className="flex flex-col justify-center ">
             {[...filteredSongs].reverse().map((song: Song) => (
               <li key={song.id} className="bg-neutral mb-5 rounded-lg">
-                <div className="flex justify-between items-center p-5">
-                  <Link className="card-body text-left overflow-hidden" href={`/home/${song.id}`}>
+                <div className="song-list-card flex justify-between items-center">
+                  <Link className="card-list-text card-body text-left overflow-hidden" href={`/home/${song.id}`}>
                     <p className="overflow-ellipsis overflow-hidden whitespace-nowrap">{song.songName}</p>
                   </Link>
 
                   <div className="flex-shrink-0 min-w-max">
                     <Link href={`/home/${song.id}/edit`}>
-                      <button className=" btn-edit btn-sm mr-5">
-                        <FontAwesomeIcon icon={faEdit} />
+                      <button className="edit-list edit-btn btn-sm mr-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                          <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                        </svg>
                       </button>
                     </Link>
 
                     <button
-                      className=" btn-delete btn-sm"
+                      className="delete-list delete-btn btn-sm"
                       onClick={() => handleDelete(song.id)}
                     >
-                      <FontAwesomeIcon icon={faTrash} />
+                      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                      </svg>
                     </button>
                   </div>
+
                 </div>
               </li>
             ))}
@@ -129,12 +134,15 @@ export default function Home() {
 
         {showConfirmation && (
           <ConfirmationModal
-            message="Are you Sure!"
+            message="Are you Sure?"
             onConfirm={handleConfirmDelete}
             onCancel={handleCancelDelete}
           />
         )}
 
+
+      </div>
+      <div className="w-full">
         <Link href="/create">
           <div className="navbar-fixed-bottom">
             <button className="fa-solid fa-circle-plus">
