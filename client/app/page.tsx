388bc4ca5cadeb5f10788546dd15fdde16b758ed
Nate Arafayne <nate.arafayne@appliedtechnology.se>
2023-07-26 10:34:57 +0200

    added Css to Starter page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,22 +1,29 @@
-"use client"
-import Link from "next/link"
+"use client";
+import Link from "next/link";
 
 export default function Start() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-100">
       <div className="hero-content flex-col lg:flex-row-reverse items-center">
-        <img src="/Lyrigator_image_1.png" className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
-          <h2 className="text-5xl font-bold">Welcome to <span className="text-accent-focus">Lyrigator</span></h2>
-          <p className="py-6">An AI powered lyric generator.</p>
-          <Link href="/home">
-            <button className="btn btn-secondary mr-10">Your Lyrics</button>
-          </Link>
-          <Link href="/create">
-            <button className="btn btn-primary">New lyrics</button>
-          </Link>
+        <img
+          src="/Lyrigator_image_1-modified.png"
+          className="lyrigator-image "
+        />
+        <div className="welcome-message">
+          <h2 className="text-warning-focus">
+            Lyrigator
+          </h2>
+          <p>AI powered lyric generator</p>
+          <div className="button-dev mt-10">
+            <Link href="/home">
+              <button className="btn btn-secondary mr-5 shadow-lg shadow-gray-700/50">Your Lyrics</button>
+            </Link>
+            <Link href="/create">
+              <button className="btn btn-primary shadow-lg shadow-gray-700/50">New lyrics</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
