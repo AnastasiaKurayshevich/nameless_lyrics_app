33b5a4241df679c672925b8e3ce7756599c0967b
Nate Arafayne <nate.arafayne@appliedtechnology.se>
2023-07-27 14:03:31 +0200

    fixed info link

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -71,10 +71,7 @@ export default function Home() {
 
   return (
     <main className="home-page flex flex-col items-center justify-start min-h-screen pt-10">
-      <div className="title-info-section">
-
-        
-
+      <div className="info-link">
         <Link href="/home/info">
           <button className="info-btn info-btn-list btn-sm">
             <svg
@@ -86,7 +83,8 @@ export default function Home() {
             </svg>
           </button>
         </Link>
-
+      </div>
+      <div className="title-info-section">
         <h2 className="your-songs-title text-5xl text-center">Your Lyrics</h2>
       </div>
 
@@ -115,14 +113,23 @@ export default function Home() {
             {[...filteredSongs].reverse().map((song: Song) => (
               <li key={song.id} className="bg-neutral mb-5 rounded-lg">
                 <div className="song-list-card flex justify-between items-center">
-                  <Link className="card-list-text card-body text-left overflow-hidden" href={`/home/${song.id}`}>
-                    <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap">{song.songName}</p>
+                  <Link
+                    className="card-list-text card-body text-left overflow-hidden"
+                    href={`/home/${song.id}`}
+                  >
+                    <p className="lyric-card-name overflow-ellipsis overflow-hidden whitespace-nowrap">
+                      {song.songName}
+                    </p>
                   </Link>
 
                   <div className="flex-shrink-0 min-w-max">
                     <Link href={`/home/${song.id}/edit`}>
                       <button className="edit-list edit-btn-list btn-sm mr-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="1em"
+                          viewBox="0 0 512 512"
+                        >
                           <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
                         </svg>
                       </button>
@@ -132,7 +139,11 @@ export default function Home() {
                       className="delete-list delete-btn-list btn-sm"
                       onClick={() => handleDelete(song.id)}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="1em"
+                        viewBox="0 0 448 512"
+                      >
                         <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                       </svg>
                     </button>
@@ -157,9 +168,7 @@ export default function Home() {
         )}
       </div>
       <div className="w-full">
-
         <div className="navbar-fixed-bottom">
-     
           <button className="fa-solid fa-circle-plus">
             <Link href="/create">
               <svg
@@ -171,9 +180,7 @@ export default function Home() {
               </svg>
             </Link>
           </button>
-
         </div>
-
       </div>
     </main>
   );
