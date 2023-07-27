deb0632b2d5ad906977796b26dfd74b10cc847fb
EdwardArvinius <edward.arvinius@appliedtechnology.se>
2023-07-27 11:33:06 +0200

    fixed styling in song display

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
 
                   <div className="flex-shrink-0 min-w-max">
                     <Link href={`/home/${song.id}/edit`}>
-                      <button className="edit-list edit-btn btn-sm mr-5">
+                      <button className="edit-list edit-btn-list btn-sm mr-5">
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                           <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
                         </svg>
@@ -114,7 +114,7 @@ export default function Home() {
                     </Link>
 
                     <button
-                      className="delete-list delete-btn btn-sm"
+                      className="delete-list delete-btn-list btn-sm"
                       onClick={() => handleDelete(song.id)}
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
